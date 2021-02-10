6dfd328b0bfc925363cab5465dc7b572ed5fe50e
nathaniaratnadewi <78688867+nathaniaratnadewi@users.noreply.github.com>
2021-02-10 13:39:49 +0700

    Tugas pemrograman python 1

---
+++ program 10.py
@@ -0,0 +1,8 @@
+Celcius=float(input("Masukkan Nilai Celcius ="))
+
+Reamur = 4/5*Celcius
+Fahrenheit = 9/5*Celcius+32
+
+print("Celcius = ",Celcius)
+print("Reamur  = ",Reamur)
+print("Fahrenheit  =",Fahrenheit)
